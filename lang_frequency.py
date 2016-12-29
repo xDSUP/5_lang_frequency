debc43ee289cf27f8241f676cb7a03dad0afd8b8
xDSUP <ilya.elfimow@yandex.ru>
2016-12-29 16:15:14 +0300

    Исправлено 1.0

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,44 +1,26 @@
-import collections
+import re
 '''
 Created on 25 12 2016
-
 @author: xD_HOHOHO
 '''
 
-print_txt = """
-Часто встречающиеся слова:
-"""
+def load_data(filepath = 'text.txt'):#открывает фаил с текстом, очищает текст от лишних символов и возвращает его программе
+    with open(filepath,'r') as file_text:
+        return re.sub(r'[`~#1234567890=\+\*\_&\?!\.\$\@,\-;:"\'\n\(\)\[\]\{\}\\/\|]',' ', file_text.read())
 
-def load_data(filepath = 'text.txt'):
-    file_txt = open(filepath,'r')
-    text = file_txt.read()
-    file_txt.close()
-    return text
-
-def get_most_frequent_words(text):
-    delimiter_characters = '`~#1234567890=+*_&?!...$@,.-;:"\'\n()[]{}\/ '
-    words = collections.defaultdict (int)
-    word = ''
-    for symvol in text:
-        if symvol not in delimiter_characters:
-            word += symvol 
-            
-        else:
-            words[word.lower()]+= 1
-            word = '' 
-    words.__delitem__('')
-    lambda_funs = lambda x: x[1]
-    frequent_words = sorted(words.items(), key=lambda_funs, reverse=True)[0:10]
-    return frequent_words 
+def get_most_frequent_words(text): 
+    all_words = re.findall(r'\w+',text)
+    words_with_the_number_of_meetings = collections.defaultdict(int) #словарь со всеми словами и колличеством в тексте
+    for word in all_words:
+        words_with_the_number_of_meetings[word.lower()]+= 1 
+    return sorted(words_with_the_number_of_meetings.items(), key=lambda x: x[1], reverse=True)[0:10]
 
 if __name__ == '__main__':
-    print("""
-
-Программа найдет самые частые слова в любом тексте.
-
-""")
+    print("Программа найдет самые частые слова в любом тексте.")
     filepath = input('Введите путь до фаила с текстом(пример: text.txt) :  ')        
-    words_and_numbers = get_most_frequent_words(text)
-    print(print_txt)
-    for num in range(0,10):
-        print(num+1, '. ', words_and_numbers[num][0], ' [', words_and_numbers[num][1], ']')
+    text = load_data(filepath)#текст в форме строки без всех знаков препинания
+    words_and_counts = get_most_frequent_words(text)#10 слов и количесво их в тексте
+    
+    print("Часто встречающиеся слова:")
+    for item in words_and_counts:
+        print(words_and_counts.index(item)+1, " '{}' встречается {} раз".format(item[0], item[1]))
