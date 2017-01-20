29cbbc42e78ed96c01b8e18ae879b67c38a2663f
xDSUP <ilya.elfimow@yandex.ru>
2017-01-20 12:31:32 +0300

    fix 2.0

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,15 +13,20 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    text = re.sub(r'[\d]', ' ', text)
-    all_words = re.split(r'\W+', text.lower())
+    treated_text = re.sub(r'[\d]', ' ', text)
+    all_words = re.split(r'\W+', treated_text.lower())
     ten_frequent_words = Counter(all_words).most_common(10)
     return ten_frequent_words
+
+
+def print_frequent_words(frequent_words):
+    print("Часто встречающиеся слова:")
+    for pair in frequent_words:
+        print("Слово '{}' встречается {} раз".format(pair[0], pair[1]))
+
 
 if __name__ == '__main__':
     filepath = input('Введите путь до фаила с текстом(пример: text.txt) :  ')
     text = load_data(filepath)
-    if text:
-        print("Часто встречающиеся слова:")
-        for pair in get_most_frequent_words(text):
-            print("Слово '{}' встречается {} раз".format(pair[0], pair[1]))
+    frequent_words = get_most_frequent_words(text)
+    print_frequent_words(frequent_words)
